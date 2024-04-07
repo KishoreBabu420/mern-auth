29e4da70d9b61620ecfc15a02907d0d7eb33b336
KishoreBabu <kishore.sai37@gmail.com>
2024-04-07 17:17:14 +0530

    "Add private route for protected pages and update header component with current user information"
    This diff adds a new file `private.route.jsx` which is used to create a private route for protected pages. It also updates the `header.component.jsx` file to include the current user's information in the header. The `App.jsx` file is updated to use the new private route for protected pages.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,9 @@ const Profile = lazy(() => import('./pages/profile.page'));
 const Login = lazy(() => import('./pages/login.page'));
 const Register = lazy(() => import('./pages/register.page'));
 
+//Private Route
+import PrivateRoute from './routes/private.route';
+
 //import Necessary Components
 import { Header } from './components';
 
@@ -31,18 +34,6 @@ export default function App() {
       {/* Use the Suspense component to handle lazy loading */}
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          {/* Define the home route */}
-          <Route
-            path='/'
-            element={<Home />}
-          />
-
-          {/* Define the about route */}
-          <Route
-            path='/about'
-            element={<About />}
-          />
-
           {/* Define the login route */}
           <Route
             path='/login'
@@ -55,11 +46,23 @@ export default function App() {
             element={<Register />}
           />
 
-          {/* Define the profile route */}
-          <Route
-            path='/profile'
-            element={<Profile />}
-          />
+          <Route element={<PrivateRoute />}>
+            {/* Define the home route */}
+            <Route
+              path='/'
+              element={<Home />}
+            />
+            {/* Define the about route */}
+            <Route
+              path='/about'
+              element={<About />}
+            />
+            {/* Define the profile route */}
+            <Route
+              path='/profile'
+              element={<Profile />}
+            />
+          </Route>
         </Routes>
       </Suspense>
     </BrowserRouter>
